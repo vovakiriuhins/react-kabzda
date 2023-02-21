5048d64db243e746d0358a31ffee0994b30054e6
vovakiriuhins <vova11999977@gmail.com>
2023-02-21 16:03:22 +0300

    splitting components (Accordion)

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,23 @@ function Star() {
 function Accordion() {
     return (
         <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    return (
+        <div>
             <h3>Menu</h3>
+        </div>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <div>
             <ul>
                 <li>1</li>
                 <li>2</li>
